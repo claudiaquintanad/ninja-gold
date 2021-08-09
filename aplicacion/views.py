7a0e7be06cb2cde3460062cf561da2ae299d5ca7
C. Alexandra <c.alexandra@Claudias-MacBook-Pro.local>
2021-08-09 13:28:22 -0400

    actividad terminada

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 import random
+from time import gmtime, strftime,localtime
 
 def index(request):
     return render(request,'index.html')
@@ -9,26 +10,26 @@ def processmoney(request):
         if request.POST['ninja'] == 'Farm': 
             earn = random.randint(10,20)
             request.session['contador'] += earn
-            request.session['salida'].append({'text': f"Earned {earn} golds from the casino!"})
+            request.session['salida'].append({'text': f"Earned {earn} golds from the casino!", 'color': 'verde', 'date': strftime("%H:%M %p", localtime())})
             request.session.save()
         if request.POST['ninja'] == 'Cave': 
             earn = random.randint(5,10)
             request.session['contador'] += earn
-            request.session['salida'].append({'text': f"Earned {earn} golds from the casino!"})
+            request.session['salida'].append({'text': f"Earned {earn} golds from the casino!", 'color': 'verde', 'date': strftime("%H:%M %p", localtime())})
             request.session.save()
         if request.POST['ninja'] == 'House': 
             earn = random.randint(2,5)
             request.session['contador'] += earn
-            request.session['salida'].append({'text': f"Earned {earn} golds from the casino!"})
+            request.session['salida'].append({'text': f"Earned {earn} golds from the casino!", 'color': 'verde', 'date': strftime("%H:%M %p", localtime())})
             request.session.save()
         if request.POST['ninja'] == 'Casino': 
             earn = random.randint(-50,50)
             request.session['contador'] += earn
             if earn > 0:
-                request.session['salida'].append({'text': f"Earned {earn} golds from the casino!"})
+                request.session['salida'].append({'text': f"Earned {earn} golds from the casino!", 'color': 'verde', 'date': strftime("%H:%M %p", localtime())})
                 request.session.save()
             else:
-                request.session['salida'].append({'text': f"Entered a casino and lost {earn} golds...Ouch..."})
+                request.session['salida'].append({'text': f"Entered a casino and lost {earn} golds...Ouch...", 'color': 'rojo', 'date': strftime("%H:%M %p", localtime())})
                 request.session.save()
 
     else:
